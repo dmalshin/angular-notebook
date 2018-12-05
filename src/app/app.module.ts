a650cb53cdd90de0d41e1433f9346f3cafa5b659
Dmitry Alshin <dm.alshin@gmail.com>
2018-12-05 14:55:05 +0300

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NewNoteComponent } from './new-note/new-note.component';
@@ -18,7 +21,9 @@ import { NotesContainerComponent } from './notes-container/notes-container.compo
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
